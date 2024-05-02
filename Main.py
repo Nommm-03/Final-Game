2f164b5afca68b31af9070ac9a5ff1ec234e4a28
Nommm-03 <133495737+Nommm-03@users.noreply.github.com>
2024-05-02 18:05:20 +0700

    Add files via upload
    Pembuatan File Main, Sprite, Config

---
+++ Main.py
@@ -0,0 +1,65 @@
+import pygame
+from sprites import *
+from config import *
+import sys
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIN_WIDTH,WIN_HEIGHT))
+        self.caption = pygame.display.set_caption('Enigma Escape')
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+    def new(self):
+        # a new game starts
+        self.playing = True
+
+        self.all_sprites = pygame.sprite.LayeredUpdates()
+        self.blocks = pygame.sprite.LayeredUpdates()
+        self.enemies = pygame.sprite.LayeredUpdates()
+        self.attacks = pygame.sprite.LayeredUpdates()
+
+        self.player = Player(self, 1, 6)
+
+    def events(self):  
+        # game loop events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.playing = False
+                self.running = False
+
+    def update(self):
+        # game loop updates
+        self.all_sprites.update()
+
+    def draw(self):
+        self.screen.fill(BLACK)
+        self.all_sprites.draw(self.screen)
+        self.clock.tick(FPS)
+        pygame.display.update()
+
+    def main(self):
+        #game loop
+        while self.playing:
+            self.events()
+            self.update()
+            self.draw()
+        self.running = False
+
+    def game_over(self):
+        pass
+
+    def intro_screen(self):
+        pass
+
+game = Game()
+game.intro_screen()
+game.new()
+while game.running:
+    game.main()
+    game.game_over()
+
+pygame.quit()
+sys.exit()
+
