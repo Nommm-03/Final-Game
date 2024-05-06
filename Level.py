2dbf5096ac75e2d2c5f1b69b5dbcf64ee6d2212e
Nommm-03 <133495737+Nommm-03@users.noreply.github.com>
2024-05-06 08:08:44 +0700

    Add files via upload

---
+++ Level.py
@@ -0,0 +1,12 @@
+import pygame
+
+class Level1:
+    def __init__(self, game):
+        self.image = 'Level/lv1.png'
+        self.game = game
+
+    def update(self):
+        pass
+
+    def movement(self):
+        pass
