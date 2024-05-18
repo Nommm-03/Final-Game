ce49ccc1af3e9b46965b5fbe9c872efa2f2e1046
Ahmad Faqih Hasani <167625310+MozaaDev@users.noreply.github.com>
2024-05-19 00:14:32 +0700

    Update key.py
    add silver key class

--- key.py
+++ key.py
@@ -19,3 +19,21 @@ class Key:
         if self.rect.colliderect(player_rect) and not self.collected:
             self.collect_sound.play()
             self.collected = True
+            
+class silver_key:
+    def __init__(self, position):
+        self.image = pygame.image.load('Level/silverkey.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (50, 50))  # Resize key image
+        self.rect = self.image.get_rect(topleft=position)
+        self.collected = False
+        self.collect_sound = pygame.mixer.Sound('Sound Effect/key2 pickup.wav')
+        self.collect_sound.set_volume(1)
+    
+    def draw(self, screen):
+        if not self.collected:
+            screen.blit(self.image, self.rect.topleft)
+
+    def collect(self, player_rect):
+        if self.rect.colliderect(player_rect) and not self.collected:
+            self.collect_sound.play()
+            self.collected = True
